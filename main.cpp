d6a6714545471d9274bced2a9c920072d05e8bf7
Emanuele Rosini <emanuele.rosini@studio.unibo.it>
2024-08-05 17:34:26 +0200

    Add random numbers generator

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include "boids.hpp"
 #include <iostream>
+#include <random>
+
 int main()
 {
-  std::vector<pf::Boid> prova;
-  pf::Point2D p1{2, 3};
-  pf::Point2D v1{0, 0};
-  pf::Point2D p2{4.5, 7.8};
-  pf::Point2D v2{0, 0};
-  pf::Boid b1{p1, v1};
-  pf::Boid b2{p2, v2};
-  prova.push_back(b1);
-  prova.push_back(b2);
- pf::Point2D v3= separazione(prova, p1, 0.5);
-  std::cout << v3.x << " "<< v3.y;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+  for (int n = 0; n < 50; ++n)
+    std::cout << dis(gen) << ' ';
+  std::cout << '\n';
 }
