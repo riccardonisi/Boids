a5a86b1fa869bd6fc417d8a3ca93e418940a10e0
Riccardo Nisi <riccardo.nisi2@studio.unibo.it>
2024-08-19 22:04:41 +0200

    Add mean_distance() and standdev_distance()

--- boids2.hpp
+++ boids2.hpp
@@ -1,10 +1,14 @@
+#ifndef BOIDS2_HPP
+#define BOIDS2_HPP
+
 #include "boids.hpp"
+#include <cmath>
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 namespace pf {
-double mean_velocity(std::vector<Boid> stormo)
+
+double mean_velocity(std::vector<Boid> const& stormo)
 {
   double sum{0};
   for (unsigned long int i{0}; i != stormo.size(); ++i) {
@@ -12,7 +16,8 @@ double mean_velocity(std::vector<Boid> stormo)
   }
   return sum / static_cast<double>(stormo.size());
 }
-double mean_position(std::vector<Boid> stormo)
+
+double mean_position(std::vector<Boid> const& stormo)
 {
   double sum{0};
   for (unsigned long int i{0}; i != stormo.size(); ++i) {
@@ -20,7 +25,8 @@ double mean_position(std::vector<Boid> stormo)
   }
   return sum / static_cast<double>(stormo.size());
 }
-double standdev_velocity(std::vector<Boid> stormo)
+
+double standdev_velocity(std::vector<Boid> const& stormo)
 {
   double sum{0};
   for (unsigned long int i{0}; i != stormo.size(); ++i) {
@@ -28,7 +34,8 @@ double standdev_velocity(std::vector<Boid> stormo)
   }
   return std::sqrt(sum / static_cast<double>(stormo.size() - 1));
 }
-double standdev_position(std::vector<Boid> stormo)
+
+double standdev_position(std::vector<Boid> const& stormo)
 {
   double sum{0};
   for (unsigned long int i{0}; i != stormo.size(); ++i) {
@@ -36,4 +43,37 @@ double standdev_position(std::vector<Boid> stormo)
   }
   return std::sqrt(sum / static_cast<double>(stormo.size() - 1));
 }
+
+double mean_distance(std::vector<Boid> const& stormo)
+{
+  double sum{0};
+  for (unsigned long int i{0}; i != stormo.size(); ++i) {
+    for (unsigned long int j{0}; j != i; ++j) {
+      sum += distanza(stormo[i].pos(), stormo[j].pos());
+    }
+  }
+  return 2.0 * sum
+       / (static_cast<double>(stormo.size())
+          * (static_cast<double>(stormo.size()) - 1));
+}
+
+double standdev_distance(std::vector<Boid> const& stormo)
+{
+  double sum{0};
+  for (unsigned long int i{0}; i != stormo.size(); ++i) {
+    for (unsigned long int j{0}; j != i; ++j) {
+      sum += pow(
+          (distanza(stormo[i].pos(), stormo[j].pos()) - mean_distance(stormo)),
+          2);
+    }
+  }
+  return std::sqrt(sum
+                   / ((static_cast<double>(stormo.size())
+                       * (static_cast<double>(stormo.size()) - 1))
+                          / 2.0
+                      - 1.0));
+}
+
 } // namespace pf
+
+#endif
