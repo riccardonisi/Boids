dce0c91e44a6b6b8349d34cc90e24246982c22a6
Riccardo Nisi <riccardo.nisi2@studio.unibo.it>
2024-08-19 22:03:40 +0200

    Change colors and variable values

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "boids.hpp"
+#include "boids2.hpp"
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <memory>
@@ -12,17 +13,16 @@ sf::Vector2f realeToPixel(float x, float y, float scaleFactorX,
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(1000, 600),
-                          "Simulazione del comportamento di uno stormo, di Nisi, Rosini, Seren");
+  sf::RenderWindow window(
+      sf::VideoMode(1000, 600),
+      "Simulazione del comportamento di uno stormo, di Nisi, Rosini, Seren");
 
   // Esempio di vettore di posizioni reali
   std::vector<pf::Boid> stormo = pf::genera_stormo(500);
 
   // Fattori di scala per la conversione
-  float scaleFactorX =
-      1000.0f / 1.0f; // Scala l'intervallo [0, 100] su [0, 800]
-  float scaleFactorY =
-      600.0f / 1.0f; // Scala l'intervallo [0, 100] su [0, 600]
+  float scaleFactorX = 1000.0f / 1.0f; // Scala l'intervallo
+  float scaleFactorY = 600.0f / 1.0f;  // Scala l'intervallo
 
   // Eseguire il loop della finestra finché è aperta
   while (window.isOpen()) {
@@ -32,22 +32,22 @@ int main()
         window.close();
     }
 
-    window.clear(sf::Color::Black);
+    window.clear(sf::Color::Cyan);
 
     // Disegnare i punti convertiti in pixel
     for (unsigned long int i{0}; i != stormo.size(); ++i) {
       sf::Vector2f pixelPos = realeToPixel(stormo[i].pos().x, stormo[i].pos().y,
                                            scaleFactorX, scaleFactorY);
-      sf::CircleShape shape(1); // Crea un cerchio di raggio 1
+      sf::CircleShape shape(2);
       shape.setPointCount(3);
       shape.setPosition(pixelPos);
-      shape.setFillColor(sf::Color::White);
+      shape.setFillColor(sf::Color::Black);
       window.draw(shape);
     }
 
     pf::movimento(stormo, 0.0025);
     pf::comportamento_bordi(stormo);
-    pf::applicazione_regole(stormo, 0.02, 0.005, 1, 0.5, 0.5);
+    pf::applicazione_regole(stormo, 0.03, 0.005, 0.85, 0.5, 0.75);
 
     window.display();
   }
