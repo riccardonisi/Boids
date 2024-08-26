fdaa4f5e75cc78bd7ce4dc1540c546f51c881d1a
Riccardo Nisi <riccardo.nisi2@studio.unibo.it>
2024-08-27 00:49:03 +0200

    Add the possibility to write mean parameters on the bash

--- main.cpp
+++ main.cpp
@@ -62,6 +62,7 @@ int main()
 
 #include "simulation.hpp"
 #include <cassert>
+#include <chrono>
 #include <iostream>
 
 int main()
@@ -71,18 +72,21 @@ int main()
   std::cout << "    ---------------------------------------------\n";
   std::cout << "      di Riccardo Nisi, Emanuele Rosini, Federico Seren\n\n";
   std::cout << "Opzioni:\n"
-            << "a. Visualizzare sul piano il comportamento di uno stormo\n";
-  std::cout << "b. Visualizzare sul piano il comportamento di uno stormo "
+            << "a. Visualizzare sul terminale l'andamento nel tempo di "
+               "distanza dall'origine, velocità dello stormo e distanza tra "
+               "gli uccelli medie\n";
+  std::cout << "b. Visualizzare sul piano il comportamento di uno stormo\n";
+  std::cout << "c. Visualizzare sul piano il comportamento di uno stormo "
                "usando i parametri consigliati\n";
-  std::cout << "c. Visualizzare l'andamento nel tempo di distanza "
-               "dall'origine, velocità dello stormo e distanza tra gli uccelli "
-               "medie con dei grafici\n";
   std::cout << "d. Visualizzare l'andamento nel tempo di distanza "
                "dall'origine, velocità dello stormo e distanza tra gli uccelli "
+               "medie con dei grafici\n";
+  std::cout << "e. Visualizzare l'andamento nel tempo di distanza "
+               "dall'origine, velocità dello stormo e distanza tra gli uccelli "
                "medie con dei grafici, usando i parametri consigliati\n";
-  std::cout << "e. Visualizzare sul piano il comportamento di due stormi (di "
+  std::cout << "f. Visualizzare sul piano il comportamento di due stormi (di "
                "specie diverse)\n";
-  std::cout << "f. Visualizzare sul piano il comportamento di due stormi "
+  std::cout << "g. Visualizzare sul piano il comportamento di due stormi "
                "usando i parametri consigliati\n";
   char op;
   std::cin >> op;
@@ -107,13 +111,51 @@ int main()
     std::cout << "Inserire l'angolo di visuale (in gradi): ";
     float angolo;
     std::cin >> angolo;
-    assert(angolo >= 0 && d <= 360);
-    pf::simulazione_piano(n, d, ds, s, a, c, angolo);
+    assert(angolo >= 0 && angolo <= 360);
+    {
+      std::vector<pf::Boid> stormo = pf::genera_stormo(n);
+      std::cout << "Inserire il numero di secondi che si vuole far durare la "
+                   "simulazione: ";
+      int time_tot;
+      std::cin >> time_tot;
+      ++time_tot;
+      std::cout << "Inserire ogni quanti secondi si desiderano leggere i "
+                   "parametri medi: ";
+      int time_lapse;
+      std::cin >> time_lapse;
+      const auto start{std::chrono::steady_clock::now()};
+      auto start2{std::chrono::steady_clock::now()};
+      std::chrono::duration<double> elapsed_seconds{0};
+      std::chrono::duration<double> elapsed_seconds2{0};
+      bool condition{true};
+      while (elapsed_seconds.count() < time_tot) {
+        if (condition == true) {
+          start2 = std::chrono::steady_clock::now();
+          std::cout << "\nTempo = " << elapsed_seconds.count() << "s\n";
+          std::cout << "Distanza dall'origine media = "
+                    << pf::mean_position(stormo) << " +/- "
+                    << pf::standdev_position(stormo) << '\n';
+          std::cout << "Velocità media = " << pf::mean_velocity(stormo)
+                    << " +/- " << pf::standdev_velocity(stormo) << '\n';
+          std::cout << "Distanza tra gli uccelli media = "
+                    << pf::mean_distance(stormo) << " +/- "
+                    << pf::standdev_distance(stormo) << '\n';
+          condition = false;
+        }
+        pf::movimento(stormo, 0.001f);
+        pf::comportamento_bordi(stormo);
+        pf::applicazione_regole(stormo, d, ds, s, a, c, angolo);
+        pf::random_boost(stormo, 0.01f, 0.01f);
+        pf::controllo_velocità(stormo, 2);
+        elapsed_seconds  = std::chrono::steady_clock::now() - start;
+        elapsed_seconds2 = std::chrono::steady_clock::now() - start2;
+        if (elapsed_seconds2.count() > time_lapse) {
+          condition = true;
+        }
+      }
+    }
     break;
   case 'b':
-    pf::simulazione_piano(200, 0.02f, 0.005f, 0.05f, 0.15f, 0.05f, 150.0f);
-    break;
-  case 'c':
     std::cout << "Inserire il numero di uccelli: ";
     std::cin >> n;
     assert(n > 1);
@@ -126,12 +168,32 @@ int main()
     assert(c > 0);
     std::cout << "Inserire l'angolo di visuale (in gradi): ";
     std::cin >> angolo;
-    pf::grafici(n, d, ds, s, a, c, angolo);
+    assert(angolo >= 0 && angolo <= 360);
+    pf::simulazione_piano(n, d, ds, s, a, c, angolo);
+    break;
+  case 'c':
+    pf::simulazione_piano(200, 0.02f, 0.005f, 0.05f, 0.15f, 0.05f, 150.0f);
     break;
   case 'd':
-    pf::grafici(20, 0.02f, 0.005f, 1.0f, 0.15f, 0.005f, 360.0f);
+    std::cout << "Inserire il numero di uccelli: ";
+    std::cin >> n;
+    assert(n > 1);
+    std::cout << "Inserire d, ds, s, a, c: ";
+    std::cin >> d >> ds >> s >> a >> c;
+    assert(d >= 0 && d <= std::sqrt(2));
+    assert(ds >= 0 && ds <= std::sqrt(2));
+    assert(s > 0);
+    assert(a > 0);
+    assert(c > 0);
+    std::cout << "Inserire l'angolo di visuale (in gradi): ";
+    std::cin >> angolo;
+    assert(angolo >= 0 && angolo <= 360);
+    pf::grafici(n, d, ds, s, a, c, angolo);
     break;
   case 'e':
+    pf::grafici(20, 0.02f, 0.005f, 1.0f, 0.15f, 0.005f, 360.0f);
+    break;
+  case 'f':
     std::cout
         << "Inserire il numero di uccelli che compongono il primo stormo: ";
     std::cin >> n;
@@ -157,10 +219,10 @@ int main()
     assert(ds2 >= 0 && ds2 <= std::sqrt(2));
     std::cout << "Inserire l'angolo di visuale (in gradi): ";
     std::cin >> angolo;
-    assert(angolo >= 0 && d <= 360);
+    assert(angolo >= 0 && angolo <= 360);
     pf::simulazione_piano_due_stormi(n, n2, d, ds, s, a, c, ds2, s2, angolo);
     break;
-  case 'f':
+  case 'g':
     pf::simulazione_piano_due_stormi(100, 100, 0.03f, 0.0025f, 0.75f, 0.5f,
                                      0.5f, 0.02f, 0.95f, 360.0f);
     break;
